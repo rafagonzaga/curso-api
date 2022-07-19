f539ef6e2a6cb1525612b60dcb04fb3795471d97
Rafael Gonzaga <ralozaga@gmail.com>
2022-07-19 12:19:49 -0300

    Classe user

--- src/main/java/com/example/testes/api/ApiApplication.java
+++ src/main/java/com/example/testes/api/ApiApplication.java
@@ -1,5 +1,6 @@
 package com.example.testes.api;
 
+import com.example.testes.api.domain.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,8 @@ public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+
+		User user = new User(1,"Rafael", "rafael@email.com", "1234");
 	}
 
 }
---
+++ src/main/java/com/example/testes/api/domain/User.java
@@ -0,0 +1,24 @@
+package com.example.testes.api.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+
+}
