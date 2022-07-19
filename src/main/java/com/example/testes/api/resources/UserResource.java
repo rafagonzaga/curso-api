057f466689be0eb7385e7492680e65f18968cce6
Rafael Gonzaga <ralozaga@gmail.com>
2022-07-19 15:39:30 -0300

    Endpoint findAll para User

--- src/main/java/com/example/testes/api/resources/UserResource.java
+++ src/main/java/com/example/testes/api/resources/UserResource.java
@@ -1,7 +1,9 @@
 package com.example.testes.api.resources;
 
 import com.example.testes.api.domain.User;
+import com.example.testes.api.domain.dto.UserDTO;
 import com.example.testes.api.services.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,16 +11,28 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping(value = "/user")
 public class UserResource {
 
     @Autowired
+    private ModelMapper mapper;
+
+    @Autowired
     private UserService service;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Integer id) {
+    public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
 
-        return ResponseEntity.ok().body(service.findById(id));
+        return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> findAll(){
+        return ResponseEntity.ok().body(service.findAll().stream().map(x -> mapper.map(x, UserDTO.class)).collect(Collectors.toList()));
+
     }
 }
--- src/main/java/com/example/testes/api/services/UserService.java
+++ src/main/java/com/example/testes/api/services/UserService.java
@@ -2,7 +2,11 @@ package com.example.testes.api.services;
 
 import com.example.testes.api.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
+
+    List<User> findAll();
 }
