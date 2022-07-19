929b4d1cc87638cb98e922ae84ecaa2179e7fdda
Rafael Gonzaga <ralozaga@gmail.com>
2022-07-19 12:32:06 -0300

    Classe UserResource e método findById

--- src/main/java/com/example/testes/api/ApiApplication.java
+++ src/main/java/com/example/testes/api/ApiApplication.java
@@ -10,7 +10,7 @@ public class ApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 
-		User user = new User(1,"Rafael", "rafael@email.com", "1234");
+//		User user = new User(1,"Rafael", "rafael@email.com", "1234");
 	}
 
 }
