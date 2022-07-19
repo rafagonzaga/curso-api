ad7545e6f974c6adaf7c03628fba04af36acdf31
Rafael Gonzaga <ralozaga@gmail.com>
2022-07-19 15:21:13 -0300

    ModelMapper e UserDTO

---
+++ src/main/java/com/example/testes/api/resources/exceptions/StandardError.java
@@ -0,0 +1,18 @@
+package com.example.testes.api.resources.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class StandardError {
+
+    private LocalDateTime timeStamp;
+    private Integer status;
+    private String error;
+    private String path;
+}
---
+++ src/main/java/com/example/testes/api/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.testes.api.services.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+}
